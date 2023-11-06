833a0d3545a18120304072302e8986b01b5ee197
Szymon Leyk <biuro@szymonleyk.pl>
2023-11-06 21:44:56 +0100

    #2 move logic from controller to service

--- src/main/kotlin/pl/szymonleyk/betgame/register/RegisterController.kt
+++ src/main/kotlin/pl/szymonleyk/betgame/register/RegisterController.kt
@@ -1,26 +1,18 @@
 package pl.szymonleyk.betgame.register
 
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import pl.szymonleyk.betgame.register.account.Account
 import pl.szymonleyk.betgame.register.account.AccountService
-import pl.szymonleyk.betgame.wallettransactions.WalletTransactionService
 import reactor.core.publisher.Mono
 
 @RestController
-class RegisterController(val accountService: AccountService, val walletTransactionService: WalletTransactionService) {
-
+class RegisterController(val accountService: AccountService) {
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    fun register(@RequestBody account: Account): Mono<Account> {
-        return accountService.add(account)
-            .flatMap { savedAccount ->
-                walletTransactionService.addEntryBalance(savedAccount.id)
-                Mono.just(savedAccount)
-            }
-    }
+    fun register(@Valid @RequestBody accountData: AccountData) : Mono<AccountId> = accountService.create(accountData)
 
 }
--- src/main/kotlin/pl/szymonleyk/betgame/register/account/AccountService.kt
+++ src/main/kotlin/pl/szymonleyk/betgame/register/account/AccountService.kt
@@ -1,8 +1,19 @@
 package pl.szymonleyk.betgame.register.account
 
 import org.springframework.stereotype.Service
+import pl.szymonleyk.betgame.register.AccountData
+import pl.szymonleyk.betgame.register.AccountId
+import pl.szymonleyk.betgame.wallettransactions.WalletTransactionService
+import reactor.core.publisher.Mono
 
 @Service
-class AccountService(val accountRepository: AccountRepository) {
-    fun add(account: Account) = accountRepository.save(account)
+class AccountService(val accountRepository: AccountRepository, val walletTransactionService: WalletTransactionService) {
+    fun create(accountData: AccountData): Mono<AccountId> = accountRepository.save(
+        Account(
+            username = accountData.username, name = accountData.name, surname = accountData.surname
+        )
+    ).flatMap { savedAccount ->
+        walletTransactionService.addEntryBalance(savedAccount.id!!)
+        Mono.just(AccountId(savedAccount.id!!))
+    }
 }
