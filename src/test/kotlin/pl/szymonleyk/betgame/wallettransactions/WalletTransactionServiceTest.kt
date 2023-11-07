5e00bb3bdf528599a20d6d27222a6311e6329e23
Szymon Leyk <biuro@szymonleyk.pl>
2023-11-07 23:25:53 +0100

    #2 add positive test for add entry balance function

---
+++ src/test/kotlin/pl/szymonleyk/betgame/wallettransactions/WalletTransactionServiceTest.kt
@@ -0,0 +1,34 @@
+package pl.szymonleyk.betgame.wallettransactions
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import pl.szymonleyk.betgame.register.account.Account
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+import java.time.LocalDate
+@ExtendWith(SpringExtension::class)
+class WalletTransactionServiceTest {
+    @Mock
+    private lateinit var walletTransactionRepository: WalletTransactionRepository
+
+    @InjectMocks
+    private lateinit var walletTransactionService:WalletTransactionService
+
+    @Test
+    fun `Test addEntryBalance`() {
+        val accountId = 1
+        val expectedTransaction = WalletTransaction(1, LocalDate.now(), WalletTransactionService.ENTRY_BALANCE, accountId)
+
+        Mockito.`when`(walletTransactionRepository.save(Mockito.any(WalletTransaction::class.java)))
+            .thenReturn(Mono.just(expectedTransaction))
+
+        StepVerifier.create(walletTransactionService.addEntryBalance(accountId))
+            .expectNext(expectedTransaction)
+            .verifyComplete()
+    }
+}
