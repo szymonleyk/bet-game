56cd83cce61ab7e6dba7981c2faa08af92c7d7e8
Szymon Leyk <biuro@szymonleyk.pl>
2023-11-10 18:00:28 +0100

    #4 add bet table ddl script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,14 +4,25 @@ CREATE TABLE `account`
     `username` VARCHAR(25) NOT NULL UNIQUE,
     `name`     VARCHAR(50) NOT NULL,
     `surname`  VARCHAR(50) NOT NULL,
-    `balance`  LONG NOT NULL DEFAULT 0
+    `balance`  LONG        NOT NULL DEFAULT 0
 );
 
 CREATE TABLE `wallet_transaction`
 (
     `id`               INTEGER PRIMARY KEY AUTO_INCREMENT,
     `transaction_date` DATE    NOT NULL,
-    `amount`           INT     NOT NULL,
+    `amount`           INTEGER NOT NULL,
     `account_id`       INTEGER NOT NULL,
+    FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+);
+
+CREATE TABLE `bet`
+(
+    `id`         INTEGER PRIMARY KEY AUTO_INCREMENT,
+    `bet_date`   DATE    NOT NULL,
+    `bet_value`  INTEGER NOT NULL,
+    `bet_number` INTEGER NOT NULL,
+    `win`        BOOLEAN,
+    `account_id` INTEGER NOT NULL,
     FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
 );
