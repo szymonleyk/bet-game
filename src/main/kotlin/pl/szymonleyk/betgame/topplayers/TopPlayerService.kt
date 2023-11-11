e49a246d34fe60abc83525f7e02219a70b30758f
Szymon Leyk <biuro@szymonleyk.pl>
2023-11-11 22:44:07 +0100

    #7 replace complicated topPlayer implementation by simple SQL script and data projection

--- src/main/kotlin/pl/szymonleyk/betgame/topplayers/TopPlayerService.kt
+++ src/main/kotlin/pl/szymonleyk/betgame/topplayers/TopPlayerService.kt
@@ -2,11 +2,9 @@ package pl.szymonleyk.betgame.topplayers
 
 import org.springframework.stereotype.Service
 import pl.szymonleyk.betgame.placebet.bet.BetRepository
-import pl.szymonleyk.betgame.register.account.AccountRepository
 import reactor.core.publisher.Flux
 
 @Service
-class TopPlayerService(val betRepository: BetRepository, val accountRepository: AccountRepository) {
-    fun fetchTopPlayers(): Flux<TopPlayer> =
-        betRepository.fetchTopPlayers()
+class TopPlayerService(val betRepository: BetRepository) {
+    fun fetchTopPlayers(): Flux<TopPlayer> = betRepository.fetchTopPlayers()
 }
