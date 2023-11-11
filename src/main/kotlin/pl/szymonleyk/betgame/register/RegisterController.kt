5bd484e6a1e75fb643a1e879b1705ac91b782b3f
Szymon Leyk <biuro@szymonleyk.pl>
2023-11-11 10:10:56 +0100

    #4 enable data validation in register controller

--- src/main/kotlin/pl/szymonleyk/betgame/register/RegisterController.kt
+++ src/main/kotlin/pl/szymonleyk/betgame/register/RegisterController.kt
@@ -1,5 +1,6 @@
 package pl.szymonleyk.betgame.register
 
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -12,6 +13,6 @@ import reactor.core.publisher.Mono
 class RegisterController(val accountService: AccountService) {
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    fun register(@RequestBody accountRequest: AccountRequest) : Mono<AccountIdData> = accountService.create(accountRequest)
+    fun register(@Valid @RequestBody accountRequest: AccountRequest) : Mono<AccountIdData> = accountService.create(accountRequest)
 
 }
