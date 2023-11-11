18f26449b8075e9af108db8215970a936fb7d9a6
Szymon Leyk <biuro@szymonleyk.pl>
2023-11-11 21:10:44 +0100

    #7 remove redundant method addEntryBalance in wallet transaction service

--- src/main/kotlin/pl/szymonleyk/betgame/wallettransactions/WalletTransactionService.kt
+++ src/main/kotlin/pl/szymonleyk/betgame/wallettransactions/WalletTransactionService.kt
@@ -1,30 +1,19 @@
 package pl.szymonleyk.betgame.wallettransactions
 
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import pl.szymonleyk.betgame.AccountNotFoundException
 import reactor.core.publisher.Flux
-import java.time.LocalDate
 
 @Service
 class WalletTransactionService(val walletTransactionRepository: WalletTransactionRepository) {
-    @Transactional(propagation = Propagation.REQUIRED)
+    @Transactional
     fun addTransaction(walletTransaction: WalletTransaction) = walletTransactionRepository.save(walletTransaction)
-
-    @Transactional(propagation = Propagation.REQUIRED)
-    fun addEntryBalance(accountId: Int) = addTransaction(
-        WalletTransaction(
-            transactionDate = LocalDate.now(), amount = ENTRY_BALANCE, accountId = accountId
-        )
-    )
 
     fun retrieveWalletTransactions(accountId: Int): Flux<WalletTransactionData> =
         walletTransactionRepository.findAllByAccountId(accountId)
             .switchIfEmpty(Flux.error(::AccountNotFoundException))
-            .map { walletTransaction ->
-                WalletTransactionData(walletTransaction.transactionDate, walletTransaction.amount)
-            }
+            .map { WalletTransactionData(it.transactionDate, it.amount) }
 
     companion object {
         const val ENTRY_BALANCE = 1000.0
--- src/test/kotlin/pl/szymonleyk/betgame/wallettransactions/WalletTransactionServiceTest.kt
+++ src/test/kotlin/pl/szymonleyk/betgame/wallettransactions/WalletTransactionServiceTest.kt
@@ -1,16 +1,11 @@
 package pl.szymonleyk.betgame.wallettransactions
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.BDDMockito.given
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import reactor.core.publisher.Mono
-import reactor.test.StepVerifier
-import java.time.LocalDate
+
 @ExtendWith(SpringExtension::class)
 class WalletTransactionServiceTest {
     @Mock
@@ -19,16 +14,4 @@ class WalletTransactionServiceTest {
     @InjectMocks
     private lateinit var walletTransactionService:WalletTransactionService
 
-    @Test
-    fun `Test addEntryBalance`() {
-        val accountId = 1
-        val expectedTransaction = WalletTransaction(1, LocalDate.now(), WalletTransactionService.ENTRY_BALANCE, accountId)
-
-        given(walletTransactionRepository.save(Mockito.any(WalletTransaction::class.java)))
-            .willReturn(Mono.just(expectedTransaction))
-
-        StepVerifier.create(walletTransactionService.addEntryBalance(accountId))
-            .expectNext(expectedTransaction)
-            .verifyComplete()
-    }
 }
